5a18ce1c5d9aa9344b2227ba5a7f64857950d0ce
Gregor Noczinski <gregor@noczinski.eu>
2017-03-19 21:25:52 +0100

    * Added more tests

--- src/main/java/org/echocat/unittest/utils/matchers/IsInstanceOf.java
+++ src/main/java/org/echocat/unittest/utils/matchers/IsInstanceOf.java
@@ -3,30 +3,36 @@ package org.echocat.unittest.utils.matchers;
 import org.hamcrest.Matcher;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
-public class IsInstanceOf extends org.hamcrest.core.IsInstanceOf {
+public class IsInstanceOf<T> extends org.hamcrest.core.IsInstanceOf {
 
     @Nonnull
-    public static <T> Matcher<T> isInstanceOf(@Nullable final Class<?> expectedType) {
+    private final Class<T> expectedType;
+
+    @Nonnull
+    public static <T> Matcher<T> isInstanceOf(@Nonnull Class<?> expectedType) {
         // noinspection unchecked
         return (Matcher<T>) new IsInstanceOf(expectedType);
     }
 
-    @SuppressWarnings("MethodOverridesStaticMethodOfSuperclass")
     @Nonnull
-    public static <T> Matcher<T> instanceOf(@Nullable final Class<?> expectedType) {
+    public static <T> Matcher<T> instanceOf(@Nonnull Class<?> expectedType) {
         return isInstanceOf(expectedType);
     }
 
-    @SuppressWarnings("MethodOverridesStaticMethodOfSuperclass")
     @Nonnull
-    public static <T> Matcher<T> any(@Nullable final Class<T> expectedType) {
+    public static <T> Matcher<T> any(@Nonnull Class<T> expectedType) {
         return isInstanceOf(expectedType);
     }
 
-    protected IsInstanceOf(@Nullable Class<?> expectedType) {
+    protected IsInstanceOf(@Nonnull Class<T> expectedType) {
         super(expectedType);
+        this.expectedType = expectedType;
+    }
+
+    @Nonnull
+    protected Class<T> expectedType() {
+        return expectedType;
     }
 
 }
---
+++ src/test/java/org/echocat/unittest/utils/matchers/IsInstanceOfUnitTest.java
@@ -0,0 +1,41 @@
+package org.echocat.unittest.utils.matchers;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matcher;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+
+import static org.echocat.unittest.utils.matchers.IsInstanceOf.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class IsInstanceOfUnitTest {
+
+    @Test
+    public void factoryMethodIsInstanceOf() throws Exception {
+        final Matcher<Object> matcher = isInstanceOf(Array.class);
+
+        assertThat(matcher, CoreMatchers.instanceOf(IsInstanceOf.class));
+        assertThat(((IsInstanceOf<?>) matcher).expectedType(), equalTo(Array.class));
+    }
+
+    @Test
+    public void factoryMethodInstanceOf() throws Exception {
+        final Matcher<Object> matcher = instanceOf(Array.class);
+
+        assertThat(matcher, CoreMatchers.instanceOf(IsInstanceOf.class));
+        assertThat(((IsInstanceOf<?>) matcher).expectedType(), equalTo(Array.class));
+    }
+
+    @Test
+    public void factoryMethodAny() throws Exception {
+        final Matcher<Array> matcher = any(Array.class);
+
+        assertThat(matcher, CoreMatchers.instanceOf(IsInstanceOf.class));
+        assertThat(((IsInstanceOf<?>) (Object) matcher).expectedType(), equalTo(Array.class));
+    }
+
+
+
+}
